da1bd11ced80a546e17a08099a3b4f7232764c4c
Christian Bouvier <cbouvier@trea.uy>
2014-08-06 10:15:49 -0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+import os
+
+setup(
+    name = "image_gallery",
+    packages = ['image_gallery',],
+
+    package_data = {
+        '': [
+            'templates/*.html',
+        ]
+    },
+
+    version = "0.1",
+    description = "Image gallery with caption and link",
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
+    author = "Trea",
+    author_email = "trea@trea.uy",
+    url = "",
+    license = "BSD",
+    keywords = ["django", "django-cms", "bootstrap", "image gallery"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Environment :: Web Environment",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Framework :: Django"
+        ],
+    include_package_data = True,
+    zip_safe = True,
+    install_requires = ['Django-CMS>=2.2'],
+    )
